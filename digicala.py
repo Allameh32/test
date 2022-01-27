cffefd3dbad47158d76c68714c2eff98c9f53a67
Allameh32 <98547098+Allameh32@users.noreply.github.com>
2022-01-27 13:40:10 -0800

    ooh

--- digicala.py
+++ digicala.py
@@ -1,21 +1,3 @@
 
 
-
-def copress(n):
-    d={}
-    for i in n:
-        d[i]=d.setdefault(i,0)+1
-    
-    k=[j for j in d.items() if j[1]>1]
-    anser=""
-    for i in k:
-        anser+="".join(str(j) for j in i)
-    return "".join(sorted(anser))
-
-n=input()
-while True:
-    com=copress(n)
-print("888888888888888888888")
-print("888888888888888888888")
-print("888888888888888888888")
-print("88888888hsfgsdfsfds8888")
+print("hhhhhhhhhhhhhhh")
